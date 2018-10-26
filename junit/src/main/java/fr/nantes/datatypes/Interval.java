e046e8828f31bb2f889a11eca0b47dd21468bd1c
Gerson Sunyé <gerson.sunye@gmail.com>
2018-10-26 15:06:29 +0200

    Added some comments

--- junit/src/main/java/fr/nantes/datatypes/Interval.java
+++ junit/src/main/java/fr/nantes/datatypes/Interval.java
@@ -8,6 +8,12 @@ public class Interval extends Range {
     private int from;
     private int to;
 
+    /**
+     * Creates an interval between two values.
+     *
+     * @param from the minimal value
+     * @param to the maximal value
+     */
     protected Interval(int from, int to) {
         assert to > from;
 
@@ -15,6 +21,12 @@ public class Interval extends Range {
         this.to = to;
     }
 
+    /**
+     * Checks whether the parameter is conainted by this internal
+     *
+     * @param i an int value.
+     * @return true, if i is between from and to.
+     */
     @Override
     public boolean includes(int i) {
         return i >= from && i <= to;
