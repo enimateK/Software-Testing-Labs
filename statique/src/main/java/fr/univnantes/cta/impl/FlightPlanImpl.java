48ba3720862cb8f37aba2996c971b1cc226fee35
Erwan Bousse <ebousse@users.noreply.github.com>
2018-11-07 17:42:56 +0100

    Add bad while loop without braces

--- statique/src/main/java/fr/univnantes/cta/impl/FlightPlanImpl.java
+++ statique/src/main/java/fr/univnantes/cta/impl/FlightPlanImpl.java
@@ -1,13 +1,12 @@
 package fr.univnantes.cta.impl;
 
-import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.List;
-import java.util.Vector;
 
 import fr.univnantes.cta.FlightPlan;
 import fr.univnantes.cta.TakenAirway;
-import java.util.LinkedList;
 
 public class FlightPlanImpl implements FlightPlan {
 
@@ -38,9 +37,9 @@ public class FlightPlanImpl implements FlightPlan {
 
     public double distance() {
         double total = 0;
-        for (TakenAirway ta : path) {
-            total = total + ta.distance();
-        }
+        Iterator<TakenAirway> it = path.iterator();
+        while (it.hasNext())
+        	total = total + it.next().distance();
         return total;
     }
 
